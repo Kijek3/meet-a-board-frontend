1abb16d5f92961849a6566129a2da2ae831d3596
Filip Szymeczko <szymeczkofil@gmail.com>
2022-04-24 13:34:39 +0200

    test: change theme test

--- src/app/service/change-theme.service.spec.ts
+++ src/app/service/change-theme.service.spec.ts
@@ -8,9 +8,23 @@ describe('ChangeThemeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ChangeThemeService);
+    service['_isLightTheme'] = true;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should change theme', () => {
+    service.switchTheme();
+
+    expect(service['_isLightTheme']).toBeTrue();
+  });
+
+  it('should change theme back', () => {
+    service.switchTheme();
+    service.switchTheme();
+
+    expect(service['_isLightTheme']).toBeFalse();
   });
 });
