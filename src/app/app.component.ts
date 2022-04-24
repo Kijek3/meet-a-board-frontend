d702f5f34bc4b3a6551fa6726bc3b56b5e3f5539
Filip Szymeczko <64635454+Kijek3@users.noreply.github.com>
2022-04-24 13:35:18 +0200

    feat: Change theme (#2)
    * feat: body background
    
    * feat: change theme and icon
    
    * test: change theme test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import { ChangeThemeService } from './service/change-theme.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Meet a Board';
+  isLightTheme: boolean;
 
   constructor(
     private primengConfig: PrimeNGConfig,
     private changeThemeService: ChangeThemeService,
-  ) {}
+  ) {
+    this.isLightTheme = changeThemeService.isLightTheme;
+  }
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
@@ -21,5 +23,6 @@ export class AppComponent implements OnInit {
 
   switchTheme(): void {
     this.changeThemeService.switchTheme();
+    this.isLightTheme = !this.isLightTheme;
   }
 }
