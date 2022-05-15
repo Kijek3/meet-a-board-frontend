d3e5766f3c9caa206734a210a843e3ad8affc139
Filip Szymeczko <64635454+Kijek3@users.noreply.github.com>
2022-05-15 14:58:11 +0200

    feat: routing, logo (#9)
    * feat: logo
    
    * feat: change theme primary color
    
    * feat: forms buttons
    
    * chore: comma
    
    * feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'login', component: LoginFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
