c6628eb1645bb825d3c774d120ebaac10faf3989
Filip Szymeczko <szymeczkofil@gmail.com>
2022-05-15 22:21:47 +0200

    test: provide configuration for unit tests

--- src/app/register-form/register-form.component.spec.ts
+++ src/app/register-form/register-form.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckboxModule } from 'primeng/checkbox';
+import { PasswordModule } from 'primeng/password';
+import { AuthService } from '../service/auth/auth.service';
 
 import { RegisterFormComponent } from './register-form.component';
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
+  let authServiceStub: Partial<AuthService>;
+  let messageServiceStub: Partial<MessageService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
+      declarations: [
+        RegisterFormComponent,
+      ],
+      imports: [
+        ReactiveFormsModule,
+        PasswordModule,
+        CheckboxModule,
+        CalendarModule,
+      ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceStub,
+        },
+        {
+          provide: MessageService,
+          useValue: messageServiceStub,
+        },
+      ],
     })
     .compileComponents();
   });
