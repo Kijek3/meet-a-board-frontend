f42a443e61d65c8025d3226bd663ea49268f8c8d
Filip Szymeczko <szymeczkofil@gmail.com>
2022-05-16 23:38:26 +0200

    test: login form

--- src/app/login-form/login-form.component.spec.ts
+++ src/app/login-form/login-form.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { PasswordModule } from 'primeng/password';
+import { of } from 'rxjs';
+import { LoginForm } from '../model/login.model';
 import { AuthService } from '../service/auth/auth.service';
 
 import { LoginFormComponent } from './login-form.component';
@@ -11,10 +13,25 @@ import { LoginFormComponent } from './login-form.component';
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
-  let authServiceStub: Partial<AuthService>;
-  let messageServiceStub: Partial<MessageService>;
+
+  let authLoginSpy: any;
+  let messageClearSpy: any;
+
+  let loginFormGroup: FormGroup;
+
+  const fields = [
+    { name: 'email', required: true },
+    { name: 'password', required: true },
+    { name: 'remember', required: false },
+  ];
 
   beforeEach(async () => {
+    const authServiceStub = jasmine.createSpyObj('AuthService', ['login']);
+    const messageServiceStub = jasmine.createSpyObj('MessageService', ['add', 'clear']);
+
+    authLoginSpy = authServiceStub.login.and.returnValue(of(null));
+    messageClearSpy = messageServiceStub.clear;
+
     await TestBed.configureTestingModule({
       declarations: [ LoginFormComponent ],
       imports: [
@@ -40,10 +57,68 @@ describe('LoginFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
+    loginFormGroup = component.loginForm;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Reactive form', () => {
+    it('should set init values in the form', () => {
+      const loginFormValues = {
+        email: '',
+        password: '',
+        remember: false,
+      };
+  
+      expect(loginFormGroup.value).toEqual(loginFormValues);
+    });
+
+    fields.filter((item) => item.required).forEach((item) => {
+      it(`should check required field '${item.name}' validator on init`, () => {
+        const loginFormGroupControl = loginFormGroup.get(item.name);
+
+        expect(loginFormGroupControl.errors).not.toBeNull();
+        expect(loginFormGroupControl.errors['required']).toBeTruthy();
+      });
+    });
+
+    it('should check email validator for email field', () => {
+      const loginFormGroupControl = loginFormGroup.get('email');
+      loginFormGroupControl.setValue('aaa');
+
+      expect(loginFormGroupControl.errors).not.toBeNull();
+      expect(loginFormGroupControl.errors['email']).toBeTruthy();
+
+      loginFormGroupControl.setValue('aaa@');
+      expect(loginFormGroupControl.errors).not.toBeNull();
+      expect(loginFormGroupControl.errors['email']).toBeTruthy();
+
+      loginFormGroupControl.setValue('aaa@dsa');
+      expect(loginFormGroupControl.errors).toBeNull();
+    });
+  });
+
+  describe('onLogin', () => {
+    it('should clear all messages in MessageService', () => {
+      component.onLogin();
+
+      expect(messageClearSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call authService with proper args', () => {
+      const formValues: LoginForm = {
+        email: 'aaa@ddd',
+        password: 'asd',
+        remember: false,
+      };
+
+      loginFormGroup.setValue(formValues);
+
+      component.onLogin();
+      expect(authLoginSpy).toHaveBeenCalledWith(formValues);
+    });
   });
 });
